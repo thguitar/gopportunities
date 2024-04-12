024133ec7c9034f27b5c92806b8c8803f268092c
Thiago.L <thiagol.guitarx@gmail.com>
2024-04-12 18:03:22 -0300

    Add router subpackage

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/thguitar/gopportunities.git/router"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	// Initialize router
+	router.Initialize()
 }
