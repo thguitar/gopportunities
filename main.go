dbb576133bba159b136a4b26d52aed27b0b1b314
Thiago.L <thiagol.guitarx@gmail.com>
2024-04-13 17:52:59 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "github.com/thguitar/gopportunities.git/router"
+import (
+	"github.com/thguitar/gopportunities.git/config"
+	"github.com/thguitar/gopportunities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
-	// Initialize router
+	// Initialize Router
 	router.Initialize()
 }
