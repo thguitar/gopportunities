3f2dfc5deca4ff89440e68078487bb3477664f6e
Thiago.L <thiagol.guitarx@gmail.com>
2024-04-18 16:57:44 -0300

    feat: add update handler logic

--- handler/request.go
+++ handler/request.go
@@ -18,7 +18,7 @@ type CreateOpeningRequest struct {
 }
 
 func (r *CreateOpeningRequest) Validate() error {
-	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Salary <= 0 {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Link == "" && r.Salary <= 0 {
 		return fmt.Errorf("request body is empty or malformed")
 	}
 	if r.Role == "" {
@@ -40,4 +40,25 @@ func (r *CreateOpeningRequest) Validate() error {
 		return errParamIsRequired("salary", "int64")
 	}
 	return nil
+}
+
+// UpdateOpening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// If any field is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// If none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+
 }
