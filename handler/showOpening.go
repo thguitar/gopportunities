99abb1aa0ab71a829d6db1ac896b442dc935eb03
Thiago.L <thiagol.guitarx@gmail.com>
2024-04-18 17:34:38 -0300

    doc: add documentation for Show Openings

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -7,6 +7,18 @@ import (
 	"github.com/thguitar/gopportunities.git/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Sumary Show opening
+// @Description Show a new job opening
+// @Tag Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} ShowOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [get]
 func ShowOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
